e3c6da4ca19fb409dcc84be767e5a941265d015e
chuprovatata <chuprova_tata@mail.ru>
2024-11-10 00:10:00 +0300

    third commit

--- src/FrequencyDictionary.java
+++ src/FrequencyDictionary.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FrequencyDictionary {
+    public static Map<Character, Integer> createFrequencyDictionary(String inputFile) throws IOException {
+        // Создаем словарь
+        Map<Character, Integer> frequencyDictionary = new HashMap<>();
+
+        // Инициализируем 0 частоту для всех строчных, а потом заглавных латинских букв
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            frequencyDictionary.put(letter, 0);
+            frequencyDictionary.put(Character.toUpperCase(letter), 0);
+        }
+
+        // Читаем символы из входного файла
+        try (BufferedReader r = new BufferedReader(new FileReader(inputFile))) {
+            int character;
+            while ((character = r.read()) != -1) {
+
+                // Проверяем, является ли символ буквой латинского алфавита
+                if (Character.isLetter(character)) {
+
+                    // Увеличиваем частоты латинских букв
+                    char curChar = (char) character;
+                    frequencyDictionary.put(curChar, frequencyDictionary.getOrDefault(curChar, 0) + 1);
+                }
+            }
+        }
+        return frequencyDictionary;
+    }
+    // Метод записи словаря в выходной файл
+    public static void writeFrequencyDictionary(Map<Character, Integer> frequencyDictionary, String outputFile) throws IOException {
+        try (FileWriter w = new FileWriter(outputFile)) {
+            for (Map.Entry<Character, Integer> entry : frequencyDictionary.entrySet()) {
+                char letter = entry.getKey();
+                if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
+                    w.write(entry.getKey() + ": " + entry.getValue() + "\n");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка записи в выходной файл: " + e.getMessage());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Map;
+import java.io.IOException;
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Вводим имя входного файла с клавиатуры
+        System.out.print("Введите имя входного файла: ");
+        String inputFile = scan.nextLine();
+
+        // Вводим имя выходного файла с клавиатуры
+        System.out.print("Введите имя выходного файла: ");
+        String outputFile = scan.nextLine();
+        try {
+            // Создаем частотный словарь для латинских букв
+            Map<Character, Integer> frequencyDictionary = FrequencyDictionary.createFrequencyDictionary(inputFile);
+
+            // Записываем этот словарь в выходной файл
+            FrequencyDictionary.writeFrequencyDictionary(frequencyDictionary, outputFile);
+        } catch (IOException e) {
+            System.err.println("Ошибка в чтении входного файла: " + e.getMessage());
         }
     }
 }
+/*
+Для тестирования созданы 3 файла. Файл Start.txt можно использовать, как пример входного файла.
+Файл находится в том же каталоге, поэтому обращаемся к файлу через относительный путь (Start.txt).
+Файл Finish.txt можно использовать, как выходной файл (тоже в рабочем каталоге). Можно создать файл
+с ограниченным доступом, если использовать его как входной или выходной файл, то получим Exception, в
+котором будет указано, что данный файл недоступен. Для чтения из файла нужен уже существующий файл,
+иначе получаем Exception. Для записи необязательно существование выходного файла, если файл с введенным
+именем не существует, то создается новый файл в рабочем каталоге с указанным пользователем именем.
+ */
